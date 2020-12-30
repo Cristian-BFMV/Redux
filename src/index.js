c9abf698c2e4662c2df32bc738a5e9b723df9435
Cristian Mendoza <ccamilo.mendoza@udea.edu.co>
2020-12-29 23:36:51 -0500

    Small changes

--- src/index.js
+++ src/index.js
@@ -2,30 +2,9 @@ const redux = require('redux');
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
 
-/*
-    Redux tiene 3 conceptos principales:
-    
-    action: Describe una acción a realziar sobre el estado 
-    de la aplicación.
-
-    reducer: Se encarga de modificar el estado de la aplicación
-    teniendo en cuenta el action que se desea realizar
-
-    store: El store es la parte central donde se almacena todo 
-    el estado de la aplicación 
-*/
-
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
-/**
- * Un action se puede generar a través de una función que retorne
- * un objeto que describa la acción a realizar sobre el estado.
- *
- * Este objeto debe contener un type, el cual describe el cambio
- * a realizar, un payload el cual contiene los datos que se le quiere
- * llevar/modificar al estado y opcionalmente puede tener cualquier
- * atributo
- */
+
 const buyCake = () => ({
   type: BUY_CAKE,
   info: 'First redux action',
@@ -43,11 +22,6 @@ const initialCakeState = {
   numOfCakes: 10,
 };
 
-/**
- * El reducer es una función que recibe como parametros el estado
- * y el action para controlar el cambio que se debe realizar sobre
- * el estado
- */
 const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
@@ -81,10 +55,6 @@ const store = createStore(rootReducer);
 
 console.log('Initial state', store.getState());
 const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
-/**
- * Con el método dispatch que provee redux, se ejecuta el reducer
- * creado, este a su vez recibe el action a ejecutar sobre el estado
- */
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
